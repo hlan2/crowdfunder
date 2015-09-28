428918084c2fc020da41cee58eab52f7d066bb76
Henry Lan <henrylan@Henrys-MacBook-Air.local>
2015-09-28 18:50:59 -0400

    Add user views, create features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :owned_projects, class_name: ‘Project’ #project creation
-	has_many :backed_projects, class_name: ‘Project’ #project backing
+	has_many :owned_projects #, class_name: ‘project’ #project creation
+	has_many :backed_projects #, class_name: ‘project’ #project backing
 	has_many :pledges
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928220254) do
+ActiveRecord::Schema.define(version: 20150928223458) do
 
   create_table "pledges", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20150928220254) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
   end
 
 end
