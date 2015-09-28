b1121a9c5f8bb02e4b8ee3ec6b61b5b6bde7c5e1
Henry Lan <henrylan@Henrys-MacBook-Air.local>
2015-09-28 19:27:19 -0400

    Enable project creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,20 @@ class ProjectsController < ApplicationController
 	def new
 		@project = Project.new
 	end
+
+	def create
+		@project = Project.new(project_params)
+
+		if @project.save
+			redirect_to projects_path
+		else
+		render :new
+		end
+	end
+
+	private
+		def project_params
+			params.require(:project).permit(:name, :description, :end_at, :start_at, :funding_goal)
+		end
+
 end
