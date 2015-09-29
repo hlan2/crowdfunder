44cc9a8bf8214739a80a99ae79e1124013f18504
Daniel Pereira <pereiradpg@yahoo.ca>
2015-09-28 23:20:33 -0400

    Add user password requirement
    Created a migration to add :password_digest column to user table.
    
    Users are required to set a password when visiting the new_user path.  Next step is to associate a
    project with its creator.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928223458) do
+ActiveRecord::Schema.define(version: 20150929030028) do
 
   create_table "pledges", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -37,10 +37,11 @@ ActiveRecord::Schema.define(version: 20150928223458) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "name"
     t.string   "email"
+    t.string   "password_digest"
   end
 
 end
