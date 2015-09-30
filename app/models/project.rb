2a521deb90f7e75d95fa95ececd465e011c08b97
Daniel Pereira <pereiradpg@yahoo.ca>
2015-09-30 18:29:29 -0400

    Project page displays data on project
    Project owner and project backer can see relation to project.
    
    Owner sees days left and amount raised, backer sees if they have pledged to project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 	belongs_to :user
 	has_many :rewards
-	has_many :pledges
+	has_many :pledges, through: :rewards
 	has_many :users, through: :pledges
 	accepts_nested_attributes_for :rewards
 end
